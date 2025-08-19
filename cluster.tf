fe2f3ba7243177359d56935f520414435aab56b9
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-19 22:48:40 +0200

    add client instance

--- cluster.tf
+++ cluster.tf
@@ -3,8 +3,8 @@ resource "aws_cloudhsm_v2_cluster" "cluster" {
   subnet_ids = [local.default_subnets["a"], local.default_subnets["b"]]
 }
 
-data "aws_cloudhsm_v2_cluster" "cluster" {
-  # by using the data source, both cluster and hsm can start deploying at the same time
-  cluster_id = aws_cloudhsm_v2_cluster.cluster.cluster_id
-  depends_on = [aws_cloudhsm_v2_hsm.hsm_one]
-}
+# data "aws_cloudhsm_v2_cluster" "cluster" {
+#   # by using the data source, both cluster and hsm can start deploying at the same time
+#   cluster_id = aws_cloudhsm_v2_cluster.cluster.cluster_id
+#   depends_on = [aws_cloudhsm_v2_hsm.hsm_one]
+# }
