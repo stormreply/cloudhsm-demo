02d87413a11075c152df3f00808347c69420ec46
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-21 08:46:25 +0200

    add cluster mode

--- cluster.tf
+++ cluster.tf
@@ -1,5 +1,6 @@
 resource "aws_cloudhsm_v2_cluster" "cluster" {
   hsm_type   = "hsm2m.medium" # only option atm, do not change
+  mode       = "NON_FIPS"
   subnet_ids = [local.default_subnets["a"], local.default_subnets["b"]]
 }
 
