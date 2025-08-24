b5e5444f993618fa2fd3815d7dd1d8d5bbee9b0e
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-24 22:50:52 +0200

    add some debug

--- 3-activate-cluster.sh
+++ 3-activate-cluster.sh
@@ -6,6 +6,11 @@ CLUSTER_ID=$1 IP_ADDRESS=$2 PASSWORD_ID=$3
 /opt/cloudhsm/bin/configure-cli -a $IP_ADDRESS
 /opt/cloudhsm/bin/cloudhsm-cli cluster activate --password "$PASSWORD_ID"
 
+ls -la /opt/cloudhsm/
+ls -la /opt/cloudhsm/etc
+ls -la /opt/cloudhsm/etc/cloudhsm-cli.cfg
+cat /opt/cloudhsm/etc/cloudhsm-cli.cfg
+
 echo "activating - can take up to 4mins"
 while [ "$CLUSTER_STATE" != "ACTIVE" ] ; do
     sleep 5
