69e15f34f37c44ca2ad310909c1fb076e49c0947
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-24 20:13:41 +0200

    remove duplicate versions

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6"
+      version = ">= 6.8.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.5.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.7.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.4"
     }
   }
   required_version = ">= 1.11.4"
