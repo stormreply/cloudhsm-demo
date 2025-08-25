d72272bb64bad0b527d2d2a2cef35ad00e83d3b6
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-26 00:39:25 +0200

    create kmsuser password

---
+++ kmsuser-password.tf
@@ -0,0 +1,24 @@
+resource "random_string" "kmsuser_password" {
+  length           = 16
+  special          = true
+  min_lower        = 4
+  min_upper        = 4
+  override_special = "!@#$%&*()-_=+[]{}<>?" # no ":"
+}
+
+# trivy:ignore:AVD-AWS-0098 (LOW): Secret explicitly uses the default key.
+resource "aws_secretsmanager_secret" "kmsuser_password" {
+  # checkov:skip=CKV_AWS_149: "Encrypt by default AWS kms key"
+  # checkov:skip=CKV2_AWS_57: "Ensure Secrets Manager secrets should have automatic rotation enabled"
+  name = "${var.deployment.name}-kmsuser-password-${random_string.suffix.result}" # needs suffix for re-creation
+}
+
+# Creating a AWS secret versions for database cluster admin account
+resource "aws_secretsmanager_secret_version" "kmsuser_password" {
+  secret_id     = aws_secretsmanager_secret.kmsuser_password.id
+  secret_string = random_string.kmsuser_password.result
+}
+
+output "kmsuser_password" {
+  value = random_string.kmsuser_password.result
+}
---
+++ passwords.tf
@@ -0,0 +1,35 @@
+locals {
+  hsm_users = toset(["admin", "kmsuser"])
+}
+
+resource "random_string" "password" {
+  for_each         = local.hsm_users
+  length           = 16
+  special          = true
+  min_lower        = 4
+  min_upper        = 4
+  override_special = "!@#$%&*()-_=+[]{}<>?" # no ":"
+}
+
+# trivy:ignore:AVD-AWS-0098 (LOW): Secret explicitly uses the default key.
+resource "aws_secretsmanager_secret" "password" {
+  # checkov:skip=CKV_AWS_149: "Encrypt by default AWS kms key"
+  # checkov:skip=CKV2_AWS_57: "Ensure Secrets Manager secrets should have automatic rotation enabled"
+  for_each = local.hsm_users
+  name     = "${var.deployment.name}-${each.key}-TO-BE-DELETED-${random_string.suffix.result}" # needs suffix for re-creation
+}
+
+# Creating a AWS secret versions for database cluster admin account
+resource "aws_secretsmanager_secret_version" "password" {
+  for_each      = local.hsm_users
+  secret_id     = aws_secretsmanager_secret.password[each.key].id
+  secret_string = random_string.password[each.key].result
+}
+
+output "admin_password" {
+  value = random_string.password["admin"].result
+}
+
+output "kmsuser_password" {
+  value = random_string.password["kmsuser"].result
+}
