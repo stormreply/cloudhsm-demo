71986708406f86d9a4236be2e5711f850a345104
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-26 12:36:01 +0200

    remove dependency to hsm two from controller instance

--- controller-instance.tf
+++ controller-instance.tf
@@ -14,7 +14,6 @@ module "controller" {
   depends_on = [
     null_resource.cluster_init,
     aws_cloudhsm_v2_hsm.hsm_one,
-    aws_cloudhsm_v2_hsm.hsm_two,
     random_string.password["admin"],
     random_string.password["kmsuser"]
   ]
