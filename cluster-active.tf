9660f803c86e2a4fd7e8757a50d6cdc0d27f2090
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-26 21:31:33 +0200

    download customerCA.crt

--- cluster-active.tf
+++ cluster-active.tf
@@ -13,6 +13,15 @@ resource "null_resource" "cluster_active" {
     }
   }
 
+  provisioner "local-exec" {
+    when    = create
+    quiet   = false
+    command = "bash ./scripts/07-copy-customer-ca-crt.sh $instance_id"
+    environment = {
+      instance_id = module.controller.instance.id
+    }
+  }
+
   depends_on = [
     aws_cloudhsm_v2_cluster.cluster
   ]
--- customer-ca-crt.tf
+++ customer-ca-crt.tf
@@ -1,6 +1,10 @@
-# data "local_file" "customer_ca_crt" {
-#   filename = "/tmp/customerCA.crt"
-#   depends_on = [
-#     null_resource.cluster_init
-#   ]
-# }
+data "local_file" "customer_ca_crt" {
+  filename = "customerCA.crt"
+  depends_on = [
+    null_resource.cluster_active
+  ]
+}
+
+output "customer_ca_crt" {
+  value = data.local_file.customer_ca_crt.content
+}
