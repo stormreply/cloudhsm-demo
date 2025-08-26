8828c9c941681d70fc9b0e38d5d8f99dbde5df77
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-26 22:02:21 +0200

    separate cluster active and customer CA key download

--- cluster-active.tf
+++ cluster-active.tf
@@ -12,17 +12,4 @@ resource "null_resource" "cluster_active" {
       cluster_id = aws_cloudhsm_v2_cluster.cluster.cluster_id
     }
   }
-
-  provisioner "local-exec" {
-    when    = create
-    quiet   = false
-    command = "bash ./scripts/07-copy-customer-ca-crt.sh $instance_id"
-    environment = {
-      instance_id = module.controller.instance.id
-    }
-  }
-
-  depends_on = [
-    aws_cloudhsm_v2_cluster.cluster
-  ]
 }
--- customer-ca-crt.tf
+++ customer-ca-crt.tf
@@ -1,7 +1,27 @@
+resource "null_resource" "cluster_ca_cert" {
+
+  triggers = {
+    name = var.deployment.name
+  }
+
+  provisioner "local-exec" {
+    when    = create
+    quiet   = false
+    command = "bash ./scripts/07-copy-customer-ca-crt.sh $instance_id"
+    environment = {
+      instance_id = module.controller.instance.id
+    }
+  }
+
+  depends_on = [
+    null_resource.cluster_active
+  ]
+}
+
 data "local_file" "customer_ca_crt" {
   filename = "customerCA.crt"
   depends_on = [
-    null_resource.cluster_active
+    null_resource.cluster_ca_cert
   ]
 }
 
