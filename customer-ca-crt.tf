6e56ab2aaee0693d44aeb0512d61a4b2c8fc85bf
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-27 08:50:22 +0200

    download clusterCA.crt

--- customer-ca-crt.tf
+++ customer-ca-crt.tf
@@ -1,27 +1,28 @@
-# resource "null_resource" "cluster_ca_cert" {
-#   triggers = {
-#     name = var.deployment.name
-#   }
-#   provisioner "local-exec" {
-#     when    = create
-#     quiet   = false
-#     command = "bash ./scripts/07-copy-customer-ca-crt.sh $instance_id"
-#     environment = {
-#       instance_id = module.controller.instance.id
-#     }
-#   }
-#   depends_on = [
-#     null_resource.cluster_active
-#   ]
-# }
+resource "null_resource" "cluster_ca_cert" {
+  triggers = {
+    name = var.deployment.name
+  }
+  provisioner "local-exec" {
+    when    = create
+    quiet   = false
+    command = "bash ./scripts/07-copy-customer-ca-crt.sh $instance_id"
+    environment = {
+      instance_id = module.controller.instance.id
+    }
+  }
+  depends_on = [
+    aws_cloudhsm_v2_hsm.hsm_two,
+    null_resource.cluster_active
+  ]
+}
 
-# data "local_file" "customer_ca_crt" {
-#   filename = "customerCA.crt"
-#   depends_on = [
-#     null_resource.cluster_ca_cert
-#   ]
-# }
+data "local_file" "customer_ca_crt" {
+  filename = "customerCA.crt"
+  depends_on = [
+    null_resource.cluster_ca_cert
+  ]
+}
 
-# output "customer_ca_crt" {
-#   value = data.local_file.customer_ca_crt.content
-# }
+output "customer_ca_crt" {
+  value = data.local_file.customer_ca_crt.content
+}
