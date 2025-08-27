c1ae574913953f80b5a8c17ec8ec1660802df98b
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-27 17:06:50 +0200

    kill ssm session explicitly

--- scripts/07-copy-customer-ca-crt.sh
+++ scripts/07-copy-customer-ca-crt.sh
@@ -17,7 +17,9 @@ aws ssm start-session \
   --target ${instance_id} \
   --document-name AWS-StartPortForwardingSession \
   --parameters '{"portNumber":["22"],"localPortNumber":["2022"]}' \
-&
+> /tmp/ssm-tunnel.log 2>&1 &
+
+SSM_PID=$!
 
 sleep 5
 
@@ -32,5 +34,10 @@ cat controller.pem
 scp -i controller.pem -o StrictHostKeyChecking=no -P 2022 ec2-user@localhost:customerCA.crt customerCA.crt
 
 echo "end SCP"
+
+kill $SSM_PID
+wait $SSM_PID || true
+
+cat /tmp/ssm-tunnel.log
 
 echo "END ---- 07-copy-customer-ca-crt.sh"
