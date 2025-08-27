4c91d4b7eec70252b193582b264abe51d4c41353
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-27 16:03:02 +0200

    suspend data source temporarily

--- customer-ca-crt.tf
+++ customer-ca-crt.tf
@@ -16,13 +16,13 @@ resource "null_resource" "cluster_ca_cert" {
   ]
 }
 
-data "local_file" "customer_ca_crt" {
-  filename = "customerCA.crt"
-  depends_on = [
-    null_resource.cluster_ca_cert
-  ]
-}
+# data "local_file" "customer_ca_crt" {
+#   filename = "customerCA.crt"
+#   depends_on = [
+#     null_resource.cluster_ca_cert
+#   ]
+# }
 
-output "customer_ca_crt" {
-  value = data.local_file.customer_ca_crt.content
-}
+# output "customer_ca_crt" {
+#   value = data.local_file.customer_ca_crt.content
+# }
