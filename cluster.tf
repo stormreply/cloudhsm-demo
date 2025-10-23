d09090a2ded1eb9e59c3bc486df231dd53b95dbb
Bernd Herding <bernd.herding@external.biontech.com>
2025-10-23 21:39:37 +0200

    add delete_cloudhsm_log_group

--- cluster.tf
+++ cluster.tf
@@ -3,3 +3,21 @@ resource "aws_cloudhsm_v2_cluster" "cluster" {
   mode       = "FIPS"
   subnet_ids = [local.default_subnets["a"], local.default_subnets["b"]]
 }
+
+# Log Group Cleanup bei Destroy
+resource "null_resource" "delete_cloudhsm_log_group" {
+  depends_on = [aws_cloudhsm_v2_cluster.cluster]
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<-EOT
+      aws logs delete-log-group \
+        --log-group-name /aws/cloudhsm/${self.triggers.cluster_id} \
+      || true
+    EOT
+  }
+
+  triggers = {
+    cluster_id = aws_cloudhsm_v2_cluster.cluster.cluster_id
+  }
+}
