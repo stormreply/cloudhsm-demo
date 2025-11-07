a374862e41e28bf497db32935d9798495762cbc0
Bernd Herding <bernd.herding@external.biontech.com>
2025-11-07 21:43:55 +0100

    exit 1 if cluster does not reach active state

--- local-exec/wait-cluster-active.sh
+++ local-exec/wait-cluster-active.sh
@@ -5,7 +5,9 @@ echo "BEGIN -- $(basename $0)"
 
 cluster_id=$1
 
-while [ "$CLUSTER_STATE" != "ACTIVE" ] ; do
+slept=0
+
+while [ "$CLUSTER_STATE" != "ACTIVE" && $slept -lt $((30 * 60))] ; do
     sleep 10
     CLUSTER_STATE=$(
     aws cloudhsmv2 describe-clusters \
@@ -14,6 +16,12 @@ while [ "$CLUSTER_STATE" != "ACTIVE" ] ; do
         --output text
     )
     echo current status: $CLUSTER_STATE
+    slept=$((slept + 10))
 done
+
+if [ "$CLUSTER_STATE" != "ACTIVE" ] ; then
+    echo "cluster $cluster_id did not transition to ACTIVE state. exiting.
+    exit 1
+fi
 
 echo "END ---- $(basename $0)"
