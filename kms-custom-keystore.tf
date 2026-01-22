45c0381cd5f2d13478f118d27eb3d68ac9b638a1
Bernd Herding <bernd.herding@external.biontech.com>
2026-01-22 11:09:19 +0100

    make custom_key_store_name more unique

--- kms-custom-keystore.tf
+++ kms-custom-keystore.tf
@@ -1,6 +1,12 @@
+locals {
+  cloud_hsm_cluster_id = aws_cloudhsm_v2_cluster.cluster.cluster_id
+  # make custom_key_store_name more unique, as it will stay if not empty
+  custom_key_store_name = "${local._name_tag}-${split("-", local.cloud_hsm_cluster_id)[1]}"
+}
+
 resource "aws_kms_custom_key_store" "cloudhsm" {
-  cloud_hsm_cluster_id     = aws_cloudhsm_v2_cluster.cluster.cluster_id
-  custom_key_store_name    = local._name_tag
+  cloud_hsm_cluster_id     = local.cloud_hsm_cluster_id
+  custom_key_store_name    = local.custom_key_store_name
   key_store_password       = random_string.password["kmsuser"].result
   trust_anchor_certificate = data.local_file.customer_ca_crt.content
 
