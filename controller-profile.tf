270544721b93504f828860a3f2e6c638a9e237c0
Bernd Herding <bernd.herding@external.biontech.com>
2026-06-08 08:18:20 +0200

    fix checkov

--- controller-profile.tf
+++ controller-profile.tf
@@ -3,6 +3,9 @@ data "aws_iam_policy_document" "controller" {
   # checkov:skip=CKV2_AWS_40: "Ensure AWS IAM policy does not allow full IAM privileges"
   # checkov:skip=CKV_AWS_49: "Ensure no IAM policies documents allow "*" as a statement's actions"
   # checkov:skip=CKV_AWS_107: "Ensure IAM policies does not allow credentials exposure"
+  # checkov:skip=CKV_AWS_108: "Ensure IAM policies does not allow data exfiltration"
+  # checkov:skip=CKV_AWS_110: "Ensure IAM policies does not allow privilege escalation"
+  # checkov:skip=CKV_AWS_111: "Ensure IAM policies does not allow write access without constraints"
   statement {
     sid    = "AllowDescribeClusters"
     effect = "Allow"
